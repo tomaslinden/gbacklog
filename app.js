d6777b038b6a26892f59049fff64edff6f32805e
tomaslinden <tomaslinden@users.noreply.github.com>
2024-08-30 13:30:52 +0300

    Further clean up (logger.js, middleware.js)
    Move logging to its own file.
    
    Also move unknownEndpoint and errorHandler methods to their own file (middleware.js)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const config = require('./utils/config')
 // const cors = require('cors')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 const app = express()
@@ -13,35 +15,20 @@ const url = config.MONGODB_URI
 
 mongoose.connect(url)
   .then(result => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
+    logger.error('error connecting to MongoDB:', error.message)
   })
 
+// Enable CORS if the Gbacklog UI is served from somewhere else
 // app.use(cors())
 app.use(express.static('dist'))
-// Enable CORS if the Gbacklog UI is served from somewhere else
 app.use(express.json())
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
 
 app.use('/api/subjects', subjectsRouter)
 
+const { unknownEndpoint, errorHandler } = middleware;
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
